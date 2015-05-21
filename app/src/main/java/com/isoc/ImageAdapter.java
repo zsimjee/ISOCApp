61e49b856b9d3682d28e3829c861ee12e36f6c79
zsimjee <zaydsimjee@gmail.com>
2015-05-20 21:46:18 -0700

    grid view

---
+++ app/src/main/java/com/isoc/ImageAdapter.java
@@ -0,0 +1,61 @@
+package com.isoc;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+/**
+ * Created by z on 5/19/2015.
+ */
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.banquet,
+            R.drawable.committee,
+            R.drawable.eid,
+            R.drawable.fitr,
+            R.drawable.general,
+            R.drawable.power,
+            R.drawable.sponsor,
+            R.drawable.today,
+            R.drawable.zakat
+    };
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+}
